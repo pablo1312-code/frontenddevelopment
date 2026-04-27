d20788c419ef6cc865eb0a3704983d7888539e99
pablo1312-code <jc0808888@gmail.com>
2026-04-27 12:29:41 +0200

    new smoke

---
+++ javascript/proyecto/afc/smoke/smoke.js
@@ -0,0 +1,16 @@
+const MIM_PARTICLE_SIZE = 0,2;
+const MIM_SPEED_X = 1;
+class Smoke{
+
+
+}
+class Particle{
+    constructor(){
+        this.size = Math.random() * 2 + MIM_PARTICLE_SIZE
+        this.color = '#542'
+        this.speedX = Math.random() * 2 + MIM_SPEED_X;
+        this.speedY = Math.random() * 2 + MIM_SPEED_Y;
+        this.x = ;
+        this.y = ;
+        }
+}
