87bc630610e72869eed09efb73901bc36c068682
pablo1312-code <jc0808888@gmail.com>
2026-04-29 14:05:36 +0200

    implementacion visual

--- javascript/proyecto/afc/smoke/smoke.js
+++ javascript/proyecto/afc/smoke/smoke.js
@@ -1,16 +1,58 @@
 const MIM_PARTICLE_SIZE = 0,2;
 const MIM_SPEED_X = 1;
-class Smoke{
+const MIM_SPEED_Y = 1;
+const NUM_OF_PARTICLES = 10;
+const canvas = document.getElementById("smoke_canvas");
+console.log(canvas);
+const ctx = canvas.getContext("2d");
+//define the color that we want to use for our drawings 
+ctx.fillStyle = rgb(53, 72, 91);
+//prepare to draw a path 
+ctx.beginPath();
+//draw an arc 
+ctx.arc(2, 3, 2, 0 Math.PI * 2);
+//fill the object that we have drawn 
+ctx.fill();
 
+
+class smoke{
+    constructor() {
+        this.particles = [];
+    }
+}
+
+add(particle){
+    this.particles.push(particle);
 
 }
 class Particle{
-    constructor(){
+    constructor(x, y){
         this.size = Math.random() * 2 + MIM_PARTICLE_SIZE
         this.color = '#542'
         this.speedX = Math.random() * 2 + MIM_SPEED_X;
         this.speedY = Math.random() * 2 + MIM_SPEED_Y;
-        this.x = ;
-        this.y = ;
+        this.x = x;
+        this.y = y;
         }
+    }
+        //update the current state of a particle 
+        update(){
+            this.sizer -= 1;
+            //this.size = this.size -1;
+
 }
+
+draw(){
+
+}
+
+window.addEventListener('mousedown',(e)=>{
+     console.log(e);
+      console.log(e.clientX);
+       console.log(e.clientY);
+
+       //create particles and add them to the smoke object 
+       for(let index = 0; index < NUM_OF_PARTICLES; index++)
+        const particle =new Particle(e.clientX, e.clientY);
+        smoke.add(particle);
+});
